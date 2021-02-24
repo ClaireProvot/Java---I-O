af8833f209e3a4dcba76ddb27020f3a375b3de0c
claire provot <claire.provot@incomlend.com>
2021-02-24 17:42:51 +0800

    create reader

--- src/org/inputouput/ReaderInAction.java
+++ src/org/inputouput/ReaderInAction.java
@@ -1,4 +1,32 @@
 package org.inputouput;
 
+import java.awt.print.Pageable;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class ReaderInAction {
+
+    public static void main(String[] args){
+
+        Path path = Paths.get("files/bat-weasels.txt");
+
+        //System.out.println(file.getName());
+        //System.out.println(file.exists());
+
+        /*File nope = new File ("files/nope.txt");
+        System.out.println(nope.getName());
+        System.out.println(nope.exists());
+        nope.createNewFile();*/
+
+        try (Stream<String> lines = Files.newBufferedReader(path).lines()) {
+
+            lines.forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
